f566ebdf82e07c447c1ad7453f2a8e9e3a1260fe
fausterlucas <lfauster@uolinc.com>
2022-01-23 22:17:52 -0300

    finalizando

--- bubble-parallel.c
+++ bubble-parallel.c
@@ -8,8 +8,81 @@ void bubbleSort(int *data, int left, int right);
 void initWorstCase(int *data, int vetSize);
 void printArray(int vetSize, int *data);
 
-void main(int argc, char *argv[])
-{
+void swap(int* array, int i, int j){
+    int temp = array[i];
+    array[i] = array[i + 1];
+    array[i + 1] = temp;
+}
+
+void bubbleSort(int *data, int left, int right){
+    int i, j;
+    for (i = left; i < right; i++)
+        for (j = left; j < right; j++)
+            if (data[j] > data[j + 1])
+                swap(data, j, j+1);
+}
+
+// Mergeia dois subarrays de dentro do data
+void merge(int *data, int vetSize, int left, int mid, int right){
+    int *dataTemp = malloc(vetSize*sizeof(int));
+
+    int left_end = mid - 1;
+    int tmp_pos = left;
+    int count = right - left + 1;
+    
+
+    //enquanto houver elemento na esquerda ou na direita ainda não visitado
+    while ((left <= left_end) && (mid <= right)){
+        if (data[left] <= data[mid]){
+            dataTemp[tmp_pos] = data[left];
+            tmp_pos++;
+            left++;
+        }
+        else{
+            dataTemp[tmp_pos] = data[mid];
+            tmp_pos++;
+            mid++;
+        }
+    }
+
+    // Se sobrou algo na esquerda
+    while (left <= left_end){
+        dataTemp[tmp_pos] = data[left];
+        left++;
+        tmp_pos++;
+    }
+
+    // Se sobrou algo na direita
+    while (mid <= right){
+        dataTemp[tmp_pos] = data[mid];
+        mid++;
+        tmp_pos++;
+    }
+
+    // copiando a array temporaria pra data
+    for (int i = 0; i < count; i++){
+        data[right] = dataTemp[right];
+        right--;
+    }
+
+    free(dataTemp);
+}
+
+void initWorstCase(int *data, int vetSize){
+    int j = 0;
+    for (int i = vetSize; i > 0; i--){
+        data[j] = i;
+        j++;
+    }
+}
+
+void printArray(int vetSize, int *data){
+	for (int i = 0; i < vetSize; i++)
+		printf(" %d;", data[i]);
+	printf("\n\n");
+}
+
+void main(int argc, char *argv[]){
     int vetSize = atoi(argv[1]); // tamanho do vetor
     int numProcs = atoi(argv[2]); // número de threads
 
@@ -33,102 +106,16 @@ void main(int argc, char *argv[])
             bubbleSort(data, left, vetSize - 1);
     }
 
-    for (int i = 1; i <= numProcs - 2; i++)
-    {
-        int middle = i * chunkSize;
-        merge(data, vetSize, 0, middle, middle + chunkSize - 1);
-    }
-    merge(data, vetSize, 0, (numProcs-1)*chunkSize, vetSize - 1);
+	for (int i = 1; i <= numProcs - 2; i++){
+		int middle = i * chunkSize;
+		merge(data, vetSize, 0, middle, middle + chunkSize - 1);
+	}
+	merge(data, vetSize, 0, (numProcs-1)*chunkSize, vetSize - 1);
 
     double stopT = omp_get_wtime();
 
-    // printArray(vetSize, data);
+    printArray(vetSize, data);
     
     double time = (stopT - startT);
     printf("%f\n", time);
-}
-
-void swap(int* array, int i, int j){
-    int temp = array[i];
-    array[i] = array[i + 1];
-    array[i + 1] = temp;
-}
-
-void bubbleSort(int *data, int left, int right)
-{
-    int i, j;
-    for (i = left; i < right; i++)
-        for (j = left; j < right; j++)
-            if (data[j] > data[j + 1])
-                swap(data, j, j+1);
-}
-
-// Mergeia dois subarrays de dentro do data
-void merge(int *data, int vetSize, int left, int mid, int right)
-{
-    int *dataTemp = malloc(vetSize*sizeof(int));
-
-    int left_end = mid - 1;
-    int tmp_pos = left;
-    int count = right - left + 1;
-    
-
-    //enquanto houver elemento na esquerda ou na direita ainda não visitado
-    while ((left <= left_end) && (mid <= right))
-    {
-        if (data[left] <= data[mid])
-        {
-            dataTemp[tmp_pos] = data[left];
-            tmp_pos++;
-            left++;
-        }
-        else
-        {
-            dataTemp[tmp_pos] = data[mid];
-            tmp_pos++;
-            mid++;
-        }
-    }
-
-    // Se sobrou algo na esquerda
-    while (left <= left_end)
-    {
-        dataTemp[tmp_pos] = data[left];
-        left++;
-        tmp_pos++;
-    }
-
-    // Se sobrou algo na direita
-    while (mid <= right)
-    {
-        dataTemp[tmp_pos] = data[mid];
-        mid++;
-        tmp_pos++;
-    }
-
-    // copiando a array temporaria pra data
-    for (int i = 0; i < count; i++)
-    {
-        data[right] = dataTemp[right];
-        right--;
-    }
-
-    free(dataTemp);
-}
-
-void initWorstCase(int *data, int vetSize)
-{
-    int j = 0;
-    for (int i = vetSize; i > 0; i--)
-    {
-        data[j] = i;
-        j++;
-    }
-}
-
-void printArray(int vetSize, int *data)
-{
-	for (int i = 0; i < vetSize; i++)
-		printf(" %d;", data[i]);
-	printf("\n\n");
 }
